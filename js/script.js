150a000899bd5d4f02a392009b7fbcaba61c1dfa
Samson Orr <171129@student.vansd.org>
2022-11-10 21:34:24 +0000

    Amp and other OK changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js");
+  }
+});
 var encEmail = "MTcxMTI5QHN0dWRlbnQudmFuc2Qub3Jn";
 const form = document.getElementById("contact");
 form.setAttribute("href", "mailto:".concat(atob(encEmail)));
